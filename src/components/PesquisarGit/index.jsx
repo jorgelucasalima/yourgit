f2e61fdcdca184f013b2e23cb086d682032034c1
jorgelucasalima <jorgelucasalima@gmail.com>
2021-12-12 14:08:27 -0300

    criado input para digitacao do nome e botao de pesquisa

--- src/components/PesquisarGit/index.jsx
+++ src/components/PesquisarGit/index.jsx
@@ -1,12 +1,19 @@
 import {Container} from './styles'
-import { FaSearch } from 'react-icons/fa'
+import { FaSearch, FaGithubAlt } from 'react-icons/fa'
 
 
 export default function PesquisarGit() {
   return (
     <Container>
-      <input type="text" />
-      <button>Buscar</button>
+      <span>Seu GitHub</span>
+      <FaGithubAlt size={27} color='#FFFFFF'/>
+      <input 
+        type="text" 
+        placeholder='Ex: @cuscuzcumshark'
+      />
+      <button>
+        <FaSearch size={20} color="#FFFFFF"/>
+      </button>
     </Container>
   )
 }
--- src/components/PesquisarGit/styles.js
+++ src/components/PesquisarGit/styles.js
@@ -7,9 +7,49 @@ export const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  
+  input {
+    margin-left: 10px;
+    text-align: left;
 
-`;  
+    padding: 1rem .875rem;
+    box-sizing: border-box; 
 
-export const Content = styled.div`
+    width: 500px;
+    height: 50px;
+
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+
+    font-size: large;
+    color: var(--cor-fonte);
+
+    border-radius: 6px;
+    border: none;
+
+    background: #202024;
+  }
+
+  button {
+    margin-left: 20px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    
+    
+    border: none;
+    color: var(--cor-fonte);
+    border-radius: 6px;
+    height: 50px;
+    width: 100px;
+    background: var(--cor-botao);
+
+    cursor: pointer;
+
+  }
+
 
 `;
